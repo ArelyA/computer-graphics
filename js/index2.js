0c001a8b509f089dcdc5e33c4065ac88f0a0b239
Jesus Mejia <jesmga24@gmail.com>
2020-11-25 10:56:44 -0600

    Loaded boos in the scene and added movement to them, also changed the room size

--- js/index2.js
+++ js/index2.js
@@ -1,4 +1,5 @@
 //import { Scene, GLTFLoader } from 'js/three.js'
+//import { GLTFLoader } from "./loaders/GLTFLoader.js";
 //Initial Configuration
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -21,15 +22,18 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix()
 })
 
-controls = new THREE.OrbitControls(camera, renderer.domElement)
+let controls = new THREE.OrbitControls(camera, renderer.domElement)
 controls.enableZoom = true
 controls.enablePan = true
 controls.maxPolarAngle = Math.PI / 2
 
-var light = new THREE.PointLight(0xffffff, 0.8)
+var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+light = new THREE.PointLight(0xffffff, 0.8)
 camera.add(light)
 
-var geometry = new THREE.BoxGeometry(20.5, 10, 0.25)
+var geometry = new THREE.BoxGeometry(30, 10, 0.25)
 var material = new THREE.MeshLambertMaterial({
   color: 0xffffff,
 })
@@ -79,8 +83,8 @@ class CustomSinCurve extends THREE.Curve {
 }
 
 //Primera pared
-mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(0, 0, 10)
+let mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(0, 0, 15)
 mesh.rotation.set(0, 0, 0)
 scene.add(mesh)
 mesh.userData.normal = new THREE.Vector3(0, 0, -1)
@@ -89,7 +93,7 @@ mesh.onAfterRender = onAfterRender
 
 //Segunda Pared
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(0, 0, -10)
+mesh.position.set(0, 0, -15)
 mesh.rotation.set(0, 0, 0)
 scene.add(mesh)
 mesh.userData.normal = new THREE.Vector3(0, 0, 1)
@@ -98,7 +102,7 @@ mesh.onAfterRender = onAfterRender
 
 //Tercera Pared
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(10, 0, 0)
+mesh.position.set(15, 0, 0)
 mesh.rotation.set(0, -Math.PI / 2, 0)
 scene.add(mesh)
 mesh.userData.normal = new THREE.Vector3(-1, 0, 0)
@@ -107,7 +111,7 @@ mesh.onAfterRender = onAfterRender
 
 //Cuarta Pared
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-10, 0, 0)
+mesh.position.set(-15, 0, 0)
 mesh.rotation.set(0, Math.PI / 2, 0)
 scene.add(mesh)
 mesh.userData.normal = new THREE.Vector3(1, 0, 0)
@@ -115,7 +119,7 @@ mesh.onBeforeRender = onBeforeRender
 mesh.onAfterRender = onAfterRender
 
 //Piso
-var geometry = new THREE.BoxGeometry(20.5, 20.5, 0.25)
+var geometry = new THREE.BoxGeometry(30, 30, 0.25)
 mesh = new THREE.Mesh(geometry, material)
 mesh.position.set(0, -5, 0)
 mesh.rotation.set(-Math.PI / 2, 0, 0)
@@ -127,69 +131,69 @@ mesh.onAfterRender = onAfterRender
 //Closet #1
 var geometry = new THREE.BoxGeometry(2, 8, 6)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(9, -1, 6)
+mesh.position.set(14, -1, 6)
 scene.add(mesh)
 //Puerta #1
 var geometry = new THREE.BoxGeometry(0.5, 7, 2.5)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(8, -1, 7.5)
+mesh.position.set(13, -1, 7.5)
 scene.add(mesh)
 //Puerta #2
 var geometry = new THREE.BoxGeometry(0.5, 7, 2.5)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(8, -1, 4.5)
+mesh.position.set(13, -1, 4.5)
 scene.add(mesh)
 //Mango de la Puerta #1
 var geometry = new THREE.SphereGeometry(0.25, 32, 32)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(7.75, -1, 5)
+mesh.position.set(12.75, -1, 5)
 scene.add(mesh)
 //Mango de la Puerta #2
 var geometry = new THREE.SphereGeometry(0.25, 32, 32)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(7.75, -1, 7)
+mesh.position.set(12.75, -1, 7)
 scene.add(mesh)
 
 //Closet #2
 var geometry = new THREE.BoxGeometry(2, 8, 6)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(9, -1, -6)
+mesh.position.set(14, -1, -6)
 scene.add(mesh)
 //Puerta #3
 var geometry = new THREE.BoxGeometry(0.5, 7, 2.5)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(8, -1, -7.5)
+mesh.position.set(13, -1, -7.5)
 scene.add(mesh)
 //Puerta #4
 var geometry = new THREE.BoxGeometry(0.5, 7, 2.5)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(8, -1, -4.5)
+mesh.position.set(13, -1, -4.5)
 scene.add(mesh)
 //Mango de la Puerta #3
 var geometry = new THREE.SphereGeometry(0.25, 32, 32)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(7.75, -1, -5)
+mesh.position.set(12.75, -1, -5)
 scene.add(mesh)
 //Mango de la Puerta #4
 var geometry = new THREE.SphereGeometry(0.25, 32, 32)
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(7.75, -1, -7)
+mesh.position.set(12.75, -1, -7)
 scene.add(mesh)
 //Cajonera
 var geometry = new THREE.BoxGeometry(3, 6, 7)
 material = new THREE.MeshBasicMaterial({ color: 0x3ccd28 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-8, -3, 0)
+mesh.position.set(-13, -3, 0)
 scene.add(mesh)
 var geometry = new THREE.BoxGeometry(0.25, 1.8, 6.3)
 material = new THREE.MeshBasicMaterial({ color: 0xe24673 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-6.5, -3.5, 0)
+mesh.position.set(-11.5, -3.5, 0)
 scene.add(mesh)
 var geometry = new THREE.BoxGeometry(0.25, 1.8, 6.3)
 material = new THREE.MeshBasicMaterial({ color: 0xe24673 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-6.5, -1.5, 0)
+mesh.position.set(-11.5, -1.5, 0)
 scene.add(mesh)
 //Tapete
 var geometry = new THREE.CylinderGeometry(8, 8, 0.25, 64)
@@ -201,41 +205,124 @@ scene.add(mesh)
 var geometry = new THREE.CylinderGeometry(0.1, 0.1, 2.8, 64)
 material = new THREE.MeshBasicMaterial({ color: 0x489ad1 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-7.5, -3, 7)
+mesh.position.set(-12.5, -3, 7)
 scene.add(mesh)
 //Palo de mesa #1
 var geometry = new THREE.CylinderGeometry(2, 2, 0.3, 64)
 material = new THREE.MeshBasicMaterial({ color: 0x489ad1 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-7.5, -1.5, 7)
+mesh.position.set(-12.5, -1.5, 7)
 scene.add(mesh)
 //Base de mesa #1
 var geometry = new THREE.CylinderGeometry(0.1, 1, 0.4, 64)
 material = new THREE.MeshBasicMaterial({ color: 0x489ad1 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-7.5, -4.3, 7)
+mesh.position.set(-12.5, -4.3, 7)
 scene.add(mesh)
 //Mesa #2
 var geometry = new THREE.CylinderGeometry(0.1, 0.1, 2.8, 64)
 material = new THREE.MeshBasicMaterial({ color: 0x489ad1 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-7.5, -3, -7)
+mesh.position.set(-12.5, -3, -7)
 scene.add(mesh)
 //Palo de mesa
 var geometry = new THREE.CylinderGeometry(2, 2, 0.3, 64)
 material = new THREE.MeshBasicMaterial({ color: 0x489ad1 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-7.5, -1.5, -7)
+mesh.position.set(-12.5, -1.5, -7)
 scene.add(mesh)
 //Base de mesa
 var geometry = new THREE.CylinderGeometry(0.1, 1, 0.4, 64)
 material = new THREE.MeshBasicMaterial({ color: 0x489ad1 })
 mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-7.5, -4.3, -7)
+mesh.position.set(-12.5, -4.3, -7)
 scene.add(mesh)
 
+
+//Cargar Boo
+var loader = new THREE.GLTFLoader();
+var boo
+var boo2
+var boo3
+var boo4
+
+loader.load( '../models/boo/scene.gltf', function ( gltf ) {
+  boo = gltf.scene.children[0];
+  boo.scale.set(0.027, 0.027, 0.027);
+  boo.position.set(0, 0, 0)
+  scene.add(boo);
+  boo2 = boo.clone();
+  boo2.scale.set(0.015, 0.015, 0.015);
+  boo2.position.set(5, 0, 5);
+  //boo2.rotation.set(0, 0, 0);
+  boo2.rotation.z = 180;
+  scene.add(boo2);
+  boo3 = boo2.clone();
+  boo3.position.set(-5, 0, 5);
+  boo3.rotation.z = 90;
+  scene.add(boo3);
+  boo4 = boo2.clone();
+  boo4.position.set(0, 0, -5);
+  boo4.rotation.z = 0;
+  scene.add(boo4);
+} );
+
+//Datos de velocidad de Boos
+var boo1Data = {
+  ySpeed: 0
+};
+
+var boo2Data = {
+  ySpeed: -0.01
+};
+
+var boo3Data = {
+  ySpeed: 0.01
+};
+
+var boo4Data = {
+  ySpeed: -0.01
+};
+
+//Funcion general de movimiento de Boos
+function moveBoos(){
+  moveBooY(boo, boo1Data);
+  moveBooY(boo2, boo2Data);
+  moveBooY(boo3, boo3Data);
+  moveBooY(boo4, boo4Data);
+  moveCircleBoo(boo2, 90);
+  moveCircleBoo(boo3, 180);
+  moveCircleBoo(boo4, 270);
+}
+
+//Movimiento de Boos en Y
+function moveBooY(boo, data){
+  if(boo.position.y <= -0.7){
+    data.ySpeed = 0.01
+  }
+  else if(boo.position.y >= 0){
+    data.ySpeed = -0.01;
+  }
+  boo.position.y += data.ySpeed;
+}
+
+//Movimiento circular de Boos
+function moveCircleBoo(boo, initPos){
+  tNueva = initPos + t;
+  boo.position.x = 8*Math.cos(tNueva) + 0;
+  boo.position.z = 8*Math.sin(tNueva) + 0; 
+  boo.rotation.z -= 0.01;
+}
+
+var t = 0;
 function animate() {
   requestAnimationFrame(animate)
+  if(boo && boo2 && boo3 && boo4){
+    t += 0.01;
+    moveBoos();
+    console.log("Posicion", boo4.rotation.z);
+    console.log(t);
+  }
   renderer.render(scene, camera)
 }
 animate()
