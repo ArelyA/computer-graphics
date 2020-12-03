73b04c7490641e5ecb23a281e6e8c5cbc9f8c110
Arely Aceves <arelyac01@gmail.com>
2020-12-02 18:10:32 -0600

    Test for Pages

--- js/index2.js
+++ js/index2.js
@@ -1,6 +1,7 @@
 //import { Scene, GLTFLoader } from 'js/three.js'
 //import { GLTFLoader } from "./loaders/GLTFLoader.js";
 
+//Imports
 import * as THREE from 'https://threejs.org/build/three.min.js';
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
